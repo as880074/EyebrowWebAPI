89499db38508875db3d101295244a078ee45d323
as880074 <as880074@gmail.com>
2022-04-24 00:25:46 +0800

    基礎建設

---
+++ EyebrowWebAPI/Eyebrow.API/Infrastructure/Mapping/ControllerProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Eyebrow.API.Infrastructure.Mapping
+{
+    public class ControllerProfile : Profile
+    {
+        public ControllerProfile()
+        {
+            
+        }
+    }
+}
